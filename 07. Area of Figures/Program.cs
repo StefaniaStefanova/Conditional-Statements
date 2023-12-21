9a556c5abf9596592b02923d09cfc2108ced95b7
StefaniaStefanova <StefaniaStefanova@users.noreply.github.com>
2023-12-21 21:31:10 +0200

    Add project files.

---
+++ 07. Area of Figures/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp = Console.ReadLine();
+            if (inp == "square")
+
+
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = a * a;
+                Console.WriteLine($"{area:F3}");
+
+            }
+
+
+            else if (inp == "rectangle")
+
+
+            {
+                double w = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                double area = w * h;
+                Console.WriteLine($"{area:F3}");
+
+            }
+
+            else if (inp == "circle")
+
+
+            {
+                double r = double.Parse(Console.ReadLine());
+                double area = Math.PI * r * r;
+                Console.WriteLine($"{area:F3}");
+
+            }
+
+            else if (inp == "triangle")
+
+
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = a * b / 2;
+                Console.WriteLine($"{area:F3}");
+
+            }
+
+
+
+        }
+    }    }  
+
